1ea617a2ddff4d83eb0e60a1501983f13a5cb7f4
Saro <57326765+khanjee501@users.noreply.github.com>
2025-08-30 20:34:00 +0500

    Feat: Added navigation between pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+import Navigation from './components/Navigation';
+
 export default function Page() {
-    return <h1>The Wild Oasis</h1>;
+    return <div>
+
+        <Navigation/>
+
+        <h1>The Wild Oasis. Welcome to Paradise!</h1>;
+
+        <Link href='/cabins'>Cabins Page</Link>
+    </div>
 }

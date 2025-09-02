93d7087127f392fb35678310240cd4df75135f6d
Saro <57326765+khanjee501@users.noreply.github.com>
2025-09-02 23:11:44 +0500

    FEAT: adding a root layout and logo

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
 import Link from 'next/link';
-import Navigation from './components/Navigation';
 
 export default function Page() {
     return <div>
-
-        <Navigation/>
 
         <h1>The Wild Oasis. Welcome to Paradise!</h1>;
 

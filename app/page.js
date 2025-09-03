6ce90b1ecbfcbe5529cf007087c8427c84b63737
Saro <57326765+khanjee501@users.noreply.github.com>
2025-09-03 15:10:50 +0500

    FEAT: Loaded and optimized fonts

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1>The Wild Oasis. Welcome to Paradise!</h1>;
+      <h1>The Wild Oasis. Welcome to Paradise!</h1>
       <Link href="/cabins">Cabins Page</Link>
     </div>
   );

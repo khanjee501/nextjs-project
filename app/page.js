7c7b924101731ae6a23615f1402d63bb6da6cbfa
Saro <57326765+khanjee501@users.noreply.github.com>
2025-09-03 15:29:22 +0500

    CHORE: improving the navigation and root layout

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1>The Wild Oasis. Welcome to Paradise!</h1>
       <Link href="/cabins">Cabins Page</Link>
     </div>
   );
